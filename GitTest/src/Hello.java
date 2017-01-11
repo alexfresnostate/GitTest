44695c8d077fb6a36366a7c4b03985b86119d534
AlexLiu <shliucsufresnoedu@gmail.com>
2017-01-11 14:42:51 -0800

    test multiple users

--- GitTest/src/Hello.java
+++ GitTest/src/Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test");
+		//from shliu
+		System.out.println("test multi user");
 	}
 
 }
